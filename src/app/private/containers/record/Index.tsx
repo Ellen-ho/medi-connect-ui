b3cf6ac545a159b326271170bd0706febae9fdbc
Elek.Chen <>
2023-07-08 11:15:48 +0800

    add global font family

--- src/app/private/containers/record/Index.tsx
+++ src/app/private/containers/record/Index.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import RecordList from './pages/RecordList';
 import CreateRecord from './pages/CreateRecord';
+import RecordHome from './pages/RecordHome';
 
 const Record: React.FC = () => {
   return (
     <Routes>
-      <Route element={<RecordList />} path="/" />
+      <Route element={<RecordHome />} path="/" />
+      <Route element={<RecordList />} path="/:typeId" />
       <Route element={<CreateRecord />} path="/new" />
     </Routes>
   );
