17be1b2c81d9d6cbed547addba44bdf545c5cb73
Ellen <ellen31725@gmail.com>
2023-07-12 01:30:45 +0800

    feat: add doctor services

---
+++ src/services/DoctorServices.ts
@@ -0,0 +1,59 @@
+import { IAddress, IDoctor } from '../types/Doctors';
+import { GenderType, MedicalSpecialtyType } from '../types/Share';
+import api from './ApiService';
+
+interface ICreateDoctorProfileRequest extends IDoctor {}
+
+interface ICreateDoctorProfileResponse {
+  id: string;
+  createdAt: Date;
+}
+
+interface IEditDoctorProfileRequest extends IDoctor {}
+
+interface IEditDoctorProfileResponse extends IDoctor {
+  id: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+interface IGetDoctorStatisticRequest {
+  doctorId: string;
+}
+
+interface IGetDoctorStatisticResponse {
+  id: string;
+  answerCounts: number;
+  thankedCounts: number;
+  beAgreedCounts: number;
+}
+
+export const createDoctorProfile = async (
+  data: ICreateDoctorProfileRequest,
+): Promise<ICreateDoctorProfileResponse> => {
+  const response = await api.post<ICreateDoctorProfileResponse>(
+    '/doctors/profile',
+    data,
+  );
+  return response.data;
+};
+
+export const editDoctorProfile = async (
+  data: IEditDoctorProfileRequest,
+): Promise<IEditDoctorProfileResponse> => {
+  const response = await api.patch<IEditDoctorProfileResponse>(
+    '/doctors/profile',
+    data,
+  );
+  return response.data;
+};
+
+export const getDoctorStatistic = async (
+  data: IGetDoctorStatisticRequest,
+): Promise<IGetDoctorStatisticResponse> => {
+  const response = await api.post<IGetDoctorStatisticResponse>(
+    '/doctors/:id/statistic',
+    data,
+  );
+  return response.data;
+};
