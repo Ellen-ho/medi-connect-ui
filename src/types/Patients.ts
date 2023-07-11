f3a54544f2b3d2528eefdb6458a79acdddd9f3f0
Ellen <ellen31725@gmail.com>
2023-07-12 01:33:11 +0800

    feat: add patients file to store the patient domain share info

---
+++ src/types/Patients.ts
@@ -0,0 +1,75 @@
+import { GenderType } from './Share';
+
+export interface IMedicalHistoryItem {
+  diagnosis: PersonalDiagnosisType;
+  diagnosisDetails?: string;
+}
+
+export enum PersonalDiagnosisType {
+  HYPERTENSION = 'HYPERTENSION',
+  DIABETES = 'DIABETES',
+  HYPERLIPIDEMIA = 'HYPERLIPIDEMIA',
+  CANCER = 'CANCER',
+  SURGERY = 'SURGERY',
+  OTHER = 'OTHER',
+}
+
+export interface IFamilyHistoryItem {
+  relationship: string;
+  diagnosis: FamilyDiagnosisType;
+  diagnosisDetails?: string;
+}
+
+export enum FamilyDiagnosisType {
+  HYPERTENSION = 'HYPERTENSION',
+  DIABETES = 'DIABETES',
+  HYPERLIPIDEMIA = 'HYPERLIPIDEMIA',
+  CANCER = 'CANCER',
+  OTHER = 'OTHER',
+}
+
+export interface IAllergy {
+  medicine: string | null;
+  food: string | null;
+  other: string | null;
+}
+
+export interface IMedicinceUsageItem {
+  medicineName: string;
+  medicineDosage: number;
+  medicineUnit: MedicineUnitType;
+  medicineFrequency: MedicineFrequencyType;
+  medicineTime: MedicineTimeType;
+}
+
+export enum MedicineUnitType {
+  MILLIGRAM = 'MILLIGRAM',
+  MILLILITER = 'MILLILITER',
+}
+
+export enum MedicineFrequencyType {
+  ONCE_DAILY = 'ONCE_DAILY',
+  TWICE_DAILY = 'TWICE_DAILY',
+  THREE_TIMES_A_DAY = 'THREE_TIMES_A_DAY',
+  FOUR_TIMES_A_DAY = 'FOUR_TIMES_A_DAY',
+  OTHER = 'OTHER',
+}
+
+export enum MedicineTimeType {
+  BEFORE_MEAL = 'BEFORE_MEAL',
+  AFTER_MEAL = 'AFTER_MEAL',
+  OTHER = 'OTHER',
+}
+
+export interface IPatient {
+  avatar: string | null;
+  firstName: string;
+  lastName: string;
+  birthDate: Date;
+  gender: GenderType;
+  medicalHistory: IMedicalHistoryItem[] | null;
+  allergy: IAllergy;
+  familyHistory: IFamilyHistoryItem[] | null;
+  heightValueCm: number;
+  medicinceUsage: IMedicinceUsageItem[] | null;
+}
