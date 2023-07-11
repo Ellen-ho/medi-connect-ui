5afc04416e69516e12eb9d172249bb290f505a42
Ellen <ellen31725@gmail.com>
2023-07-12 01:32:39 +0800

    feat: add doctors file to store the doctor domain share info

---
+++ src/types/Doctors.ts
@@ -0,0 +1,26 @@
+import { GenderType, MedicalSpecialtyType } from './Share';
+
+export interface IAddress {
+  line1: string; // Street address, P.O. Box, company name, c/o
+  line2?: string; // Apartment, suite, unit, building, floor, etc.
+  city: string; // City, town, village, etc.
+  stateProvince?: string; // State, province, region, etc.
+  postalCode?: string; // Postal code, ZIP code, etc.
+  country: string; // Country name or ISO country code
+  countryCode: string; // ISO country code (2 or 3 characters), optional if using the full country name
+}
+
+export interface IDoctor {
+  avatar: string | null;
+  firstName: string;
+  lastName: string;
+  gender: GenderType;
+  aboutMe: string;
+  languagesSpoken: string[];
+  specialties: MedicalSpecialtyType[];
+  careerStartDate: Date;
+  officePracticalLocation: IAddress;
+  education: string[];
+  awards: string[] | null;
+  affiliations: string[] | null;
+}
