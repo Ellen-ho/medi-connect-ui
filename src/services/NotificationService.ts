4eb0d7bf0adb3a3d6e7a3c21e20d7f82da88cd10
Ellen <ellen31725@gmail.com>
2023-07-12 22:30:32 +0800

    feat: add types and services of notification

---
+++ src/services/NotificationService.ts
@@ -0,0 +1,100 @@
+import queryString from 'query-string';
+import { INotification, NotificationType } from '../types/Notifications';
+import api from './ApiService';
+
+interface IDeleteAllNotificationsResponse {
+  deletedAt: Date;
+}
+
+interface IDeleteNotificationRequest {
+  notificationId: string;
+}
+
+interface IDeleteNotificationResponse {
+  deletedAt: Date;
+}
+
+interface IGetNotificationDetailsRequest {
+  notificationId: string;
+}
+
+interface IGetNotificationDetailsResponse extends INotification {}
+
+interface IGetNotificationHintsResponse {
+  hasUnReadNotification: boolean;
+}
+
+interface IGetNotificationListRequest {
+  query: {
+    page: number;
+    limit: number;
+  };
+}
+
+interface IGetNotificationListResponse {
+  data: Array<INotification> | null;
+  pagination: {
+    pages: number[];
+    totalPage: number;
+    currentPage: number;
+    prev: number;
+    next: number;
+  };
+}
+
+interface IReadAllNotificationsResponse {
+  updatedNotifications: Array<INotification>;
+}
+
+export const deleteAllNotifications =
+  async (): Promise<IDeleteAllNotificationsResponse> => {
+    const response = await api.delete<IDeleteAllNotificationsResponse>(
+      '/notifications/all',
+    );
+    return response.data;
+  };
+
+export const deleteNotification = async (
+  data: IDeleteNotificationRequest,
+): Promise<IDeleteNotificationResponse> => {
+  const response = await api.delete<IDeleteNotificationResponse>(
+    '/notifications/:id',
+  );
+  return response.data;
+};
+
+export const getNotificationHints =
+  async (): Promise<IGetNotificationHintsResponse> => {
+    const response = await api.patch<IGetNotificationHintsResponse>(
+      '/notifications/hints',
+    );
+    return response.data;
+  };
+
+export const getNotificationDetails = async (
+  data: IGetNotificationDetailsRequest,
+): Promise<IGetNotificationDetailsResponse> => {
+  const response = await api.patch<IGetNotificationDetailsResponse>(
+    '/notifications/:id',
+    data,
+  );
+  return response.data;
+};
+
+export const getNotificationList = async (
+  data: IGetNotificationListRequest,
+): Promise<IGetNotificationListResponse> => {
+  const queries = queryString.stringify(data.query);
+  const response = await api.get<IGetNotificationListResponse>(
+    `/notifications?${queries}`,
+  );
+  return response.data;
+};
+
+export const readAllNotifications =
+  async (): Promise<IReadAllNotificationsResponse> => {
+    const response = await api.patch<IReadAllNotificationsResponse>(
+      '/notifications/read-all',
+    );
+    return response.data;
+  };
