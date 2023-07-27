9bbe4616bcd708214322492ffac649414a1d7906
Elek.Chen <>
2023-07-27 23:19:16 +0800

    comment out appointment routing for now

--- src/app/private/containers/appointment/Index.tsx
+++ src/app/private/containers/appointment/Index.tsx
@@ -1,11 +1,10 @@
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import AppointmentList from './pages/AppointmentList';
-import EditDoctorTimeSlot from './pages/EditDoctorTimeSlot';
 
 const Appointment: React.FC = () => {
   return (
     <Routes>
-      <Route element={<EditDoctorTimeSlot />} path="/time-slot" />
+      {/* <Route element={<EditDoctorTimeSlot />} path="/time-slot" /> */}
       <Route element={<AppointmentList />} path="/" />
     </Routes>
   );
