f7b50c7eade2601e581f86f639050d686b473db8
Elek.Chen <>
2023-07-30 14:25:13 +0800

    fix: doctor list styles

--- src/app/private/containers/doctors/pages/DoctorList.styled.ts
+++ src/app/private/containers/doctors/pages/DoctorList.styled.ts
@@ -5,7 +5,7 @@ export const DoctorListWrapper = styled.div`
   ${mq({
     display: 'flex',
     flexDirection: ['column', 'column', 'row'],
-    alignItems: 'center',
+    alignItems: 'top',
     gap: '1rem',
     '> *': {
       width: ['100%', '50%', '50%'],
