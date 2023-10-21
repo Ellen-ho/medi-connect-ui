f43cc7201e3f5d00d4c47bb71adecfcaad97ffc4
Ellen <ellen31725@gmail.com>
2023-10-22 00:08:42 +0800

    feat: add getting data of goal duration

--- src/app/private/containers/goal/Index.tsx
+++ src/app/private/containers/goal/Index.tsx
@@ -1,24 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import HealthGoalList from './pages/HealthGoalList';
 import HealthGoalDetail from './pages/HealthGoalDetail';
-import Chart from './pages/Chart';
+import HealthGoalChart from './pages/HealthGoalChart';
 
 const HealthGoal: React.FC = () => {
   return (
     <Routes>
-      <Route
-        element={
-          <Chart
-            recordsData={{
-              bloodPressure: BloodPressureRecord,
-              bloodSugar: BloodSugarRecord,
-              glycatedHemoglobin: GlycatedHemoglobinRecord,
-              weight: WeightRecord,
-            }}
-          />
-        }
-        path="/:id/dataAnalization"
-      />
+      <Route element={<HealthGoalChart />} path="/:id/data-analysis" />
       <Route element={<HealthGoalDetail />} path="/:id" />
       <Route element={<HealthGoalList />} path="/" />
     </Routes>
