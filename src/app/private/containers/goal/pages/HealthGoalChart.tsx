fa8405d6f9d9545bafe6c651f195483831160cc1
Ellen <ellen31725@gmail.com>
2023-10-22 00:25:50 +0800

    feat: add four kind of record data to chart

--- src/app/private/containers/goal/pages/HealthGoalChart.tsx
+++ src/app/private/containers/goal/pages/HealthGoalChart.tsx
@@ -35,47 +35,156 @@ const HealthGoalChart: React.FC = () => {
   );
 
   return (
-    <Card>
-      <CardContent>
-        <Typography variant="h6" gutterBottom>
-          Health Records
-        </Typography>
-        data
-        <ResponsiveContainer width="100%" height={300}>
-          {data ? (
-            <LineChart
-              data={data.bloodPressureRecordsData}
-              margin={{
-                top: 5,
-                right: 30,
-                left: 20,
-                bottom: 5,
-              }}
-            >
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="bloodPressureDate" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Line
-                type="monotone"
-                dataKey="systolicBloodPressure"
-                name="Systolic BP"
-                stroke="#8884d8"
-              />
-              <Line
-                type="monotone"
-                dataKey="diastolicBloodPressure"
-                name="Diastolic BP"
-                stroke="#82ca9d"
-              />
-            </LineChart>
-          ) : (
-            <NoDataFound />
-          )}
-        </ResponsiveContainer>
-      </CardContent>
-    </Card>
+    <>
+      <Card>
+        <CardContent>
+          <Typography variant="h6" gutterBottom>
+            Blood Pressure Records
+          </Typography>
+          <ResponsiveContainer width="100%" height={300}>
+            {data ? (
+              <LineChart
+                data={data.bloodPressureRecordsData}
+                margin={{
+                  top: 5,
+                  right: 30,
+                  left: 20,
+                  bottom: 5,
+                }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="bloodPressureDate" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line
+                  type="monotone"
+                  dataKey="systolicBloodPressure"
+                  name="Systolic BP"
+                  stroke="#8884d8"
+                />
+                <Line
+                  type="monotone"
+                  dataKey="diastolicBloodPressure"
+                  name="Diastolic BP"
+                  stroke="#82ca9d"
+                />
+              </LineChart>
+            ) : (
+              <NoDataFound />
+            )}
+          </ResponsiveContainer>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardContent>
+          <Typography variant="h6" gutterBottom>
+            Blood Sugar Records
+          </Typography>
+          <ResponsiveContainer width="100%" height={300}>
+            {data ? (
+              <LineChart
+                data={data.bloodSugarRecordsData}
+                margin={{
+                  top: 5,
+                  right: 30,
+                  left: 20,
+                  bottom: 5,
+                }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="bloodSugarDate" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line
+                  type="monotone"
+                  dataKey="bloodSugarValue"
+                  name="Blood Sugar Value"
+                  stroke="#8884d8"
+                />
+              </LineChart>
+            ) : (
+              <NoDataFound />
+            )}
+          </ResponsiveContainer>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardContent>
+          <Typography variant="h6" gutterBottom>
+            Glycated HemoGlobin Records
+          </Typography>
+          <ResponsiveContainer width="100%" height={300}>
+            {data ? (
+              <LineChart
+                data={data.glycatedHemoglobinRecordsData}
+                margin={{
+                  top: 5,
+                  right: 30,
+                  left: 20,
+                  bottom: 5,
+                }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="glycatedHemoglobinDate" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line
+                  type="monotone"
+                  dataKey="glycatedHemoglobinValue"
+                  name="Glycated Hemoglobin Value"
+                  stroke="#8884d8"
+                />
+              </LineChart>
+            ) : (
+              <NoDataFound />
+            )}
+          </ResponsiveContainer>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardContent>
+          <Typography variant="h6" gutterBottom>
+            Weight Records
+          </Typography>
+          <ResponsiveContainer width="100%" height={300}>
+            {data ? (
+              <LineChart
+                data={data.weightRecordsData}
+                margin={{
+                  top: 5,
+                  right: 30,
+                  left: 20,
+                  bottom: 5,
+                }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="weightDate" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line
+                  type="monotone"
+                  dataKey="weightValueKg"
+                  name="Weight Value Kg"
+                  stroke="#8884d8"
+                />
+                <Line
+                  type="monotone"
+                  dataKey="bodyMassIndex"
+                  name="BMI"
+                  stroke="#82ca9d"
+                />
+              </LineChart>
+            ) : (
+              <NoDataFound />
+            )}
+          </ResponsiveContainer>
+        </CardContent>
+      </Card>
+    </>
   );
 };
 
