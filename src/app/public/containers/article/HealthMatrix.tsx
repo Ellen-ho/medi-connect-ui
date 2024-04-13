10744871d318b01633c10f6c47ab2a971d5dcbc6
Ellen <ellen31725@gmail.com>
2024-04-14 00:21:17 +0800

    feat: add health matrix page

---
+++ src/app/public/containers/article/HealthMatrix.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Typography, Grid, Box, IconButton } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
+interface SectionProps {
+  title: string;
+  content: string[];
+}
+
+const Section: React.FC<SectionProps> = ({ title, content }) => (
+  <Box style={{ marginBottom: 16 }}>
+    <Typography variant="h5" component="div">
+      {title}
+    </Typography>
+    {content.map((text, index) => (
+      <Typography variant="body1" paragraph key={index}>
+        {text}
+      </Typography>
+    ))}
+  </Box>
+);
+
+const HealthMatrixData: React.FC = () => {
+  const navigate = useNavigate();
+  return (
+    <Grid container spacing={2} padding={2}>
+      <Grid item xs={12}>
+        <IconButton onClick={() => navigate('/')} aria-label="back">
+          <ArrowBackIcon />
+        </IconButton>
+        <Section
+          title="Blood Pressure"
+          content={[
+            'Importance: Blood pressure is crucial for health as it measures the force of blood against artery walls and can indicate risks for heart disease and stroke.',
+            'Systolic and Diastolic Pressures: Systolic pressure (the top number) indicates how much pressure your blood exerts against artery walls when the heart beats, while diastolic pressure (the bottom number) measures this pressure between beats. Both readings are essential for assessing cardiovascular health risks.',
+          ]}
+        />
+        <Section
+          title="Blood Sugar"
+          content={[
+            'Importance: Blood sugar levels are vital indicators of metabolic health, influencing the risk of diabetes, heart disease, and other metabolic syndromes.',
+            'Fasting Blood Sugar and Glycated Hemoglobin (HbA1c): Fasting blood sugar tests your glucose levels after an overnight fast, which can help detect diabetes and prediabetes. HbA1c provides an average blood sugar level over the past three months, showing how well your body manages blood glucose over time.',
+          ]}
+        />
+        <Section
+          title="Body Weight"
+          content={[
+            'Importance: Body weight impacts numerous health aspects, including risks for heart disease, diabetes, and joint problems.',
+            'Body Mass Index (BMI): BMI is a ratio of your weight to height. It is a common measure to classify underweight, normal weight, overweight, and obesity, which are critical indicators of overall health risks.',
+          ]}
+        />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default HealthMatrixData;
